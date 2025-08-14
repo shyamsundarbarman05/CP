b0f86f2bda2b754d9172c6b94a42c57998cbc9b5
Shyamsundar Barman <shyamsundarbarman373@gmail.com>
2025-08-14 10:47:19 +0530

    updated

--- Binary_search.cpp
+++ Binary_search.cpp
@@ -39,3 +39,51 @@ int main()
 
 
 
+// 📌 Algorithm Name: Linear Search Algorithm
+
+// 📖 Definition:
+// Linear Search is a simple searching technique used to find the position of a specific element (key) in an array by checking each element one by one from start to end until the element is found or the array ends.
+
+// 🔹 Input:
+
+// size → number of elements in the array
+
+// n[] → array elements
+
+// key → element to be searched
+
+// 🔹 Output:
+
+// Index position of the key if found
+
+// Message “Element not found” if the key is not present
+
+// 🛠 Steps:
+
+// Step 1: Start
+
+// Step 2: Input size (number of elements in the array).
+
+// Step 3: Declare an array n[size].
+
+// Step 4: Input the elements of the array.
+
+// Step 5: Input key (element to search).
+
+// Step 6: Set i = 0.
+
+// Step 7: Repeat until i < size
+
+// If n[i] == key → Return index i (element found).
+
+// Else → Increment i.
+
+// Step 8: If end of array is reached without finding the key → Return -1 (element not found).
+
+// Step 9: Display result:
+
+// If index is not -1 → print position.
+
+// Else → print “Element not found.”
+
+// Step 10: Stop
